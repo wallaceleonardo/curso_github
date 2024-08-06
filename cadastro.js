dc1f94ede291dedad0d9494161b853002102b9ec
wallacelt <wallace.tolentino@gmail.com>
2024-08-06 11:31:58 -0300

    Adicionando funcionalidade de mostrar mensagem

--- cadastro.js
+++ cadastro.js
@@ -5,3 +5,10 @@ function signUp(email){
 
 
 // pegando o email e cadastrando na plataforma
+
+function mostraMensagem(){
+    return "Bem vindo ao sistema";
+}
+
+
+// adicionando funcao de mensagem
